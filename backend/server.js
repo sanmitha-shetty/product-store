56f9c2d72b229cdf4e198510fdfd2127704fef6e
Sanmitha Shetty <sanmithasadanand@gmail.com>
2024-09-23 13:18:03 -0500

    Create new route and controller functions

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import Product from './models/product.model.js';
-import mongoose from 'mongoose';
+import productRoutes from "./routes/product.route.js"
+
 
 dotenv.config()
 
@@ -10,64 +10,7 @@ const app = express();
 
 app.use(express.json()); // allows us to us json data in the req.body
 
-app.get("/api/products", async (req,res) => {
-    try {
-        const products = await Product.find({}); // find({}) here {} --> find all products
-        res.status(200).json({succes: true, data: products});
-    } catch (error) {
-        console.error("Error in Fetching Products", error.message);
-        res.status(500).json({success: false, message: "Server Error"});
-    }
-});
-
-app.post("/api/products", async (req,res) =>{
-    const product = req.body; // user will send this data
-
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({succes: false, message: "Please provide all fields"})
-    }
-
-    const newProduct = new Product(product)
-
-    try {
-        await newProduct.save();
-        res.status(201).json({succes: true, data: newProduct});
-    } catch (error) {
-        console.error("Error in Create Product", error.message);
-        res.status(500).json({success: false, message: "Server Error"});
-    }
-});
-
-app.delete("/api/products/:id", async (req,res) => {
-    const {id} = req.params;
-    try {
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({success: true, message: "Product Deleted"})
-    } catch (error) {
-        console.error("Error in Deleting Product", error.message);
-        res.status(404).json({success: false, message: "Product Not Found"})
-    }
-});
-
-// Use Patch method when updating a few fields
-// Use Put method when updating all the fields
-
-app.put("/api/products/:id", async (req,res) =>{
-    const {id} = req.params;
-
-    const product = req.body;
-
-    if(!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({success: false, message: "Invalid Product Id"});
-    }
-
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new:true});
-        res.status(200).json({success: true, data: updatedProduct});
-    } catch (error) {
-        res.status(500).json({success: false, message: "Server Error"});
-    }
-});
+app.use("/api/products", productRoutes )
 
 app.listen(5001, () => {
     connectDB();
